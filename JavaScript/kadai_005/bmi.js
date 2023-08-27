4517fa7a554af21f7be90b54c5f0dbf2a099c176
yusei-shinkai <140622972+yusei-shinkai@users.noreply.github.com>
2023-08-27 22:55:59 +0900

    kadai_005アップロード

---
+++ JavaScript/kadai_005/bmi.js
@@ -0,0 +1,13 @@
+let userHeight;
+console.log(userHeight);
+
+let userWeight;
+console.log(userWeight);
+
+userHeight = 1.7 
+userWeight = 68
+
+const bmi = userWeight / (userHeight * userHeight);
+
+console.log(bmi)
+
