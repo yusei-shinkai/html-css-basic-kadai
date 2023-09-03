8c53e00be0165f0d42e39a15869d87c7994a4f42
yusei-shinkai <140622972+yusei-shinkai@users.noreply.github.com>
2023-09-03 20:45:54 +0900

    kadai_009 アップロード

---
+++ JavaScript/kadai_009/holiday.js
@@ -0,0 +1,10 @@
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"] 
+console.log(holidays);
+
+while (holidays == 0 ) ;
+console.log("本日は" + holidays + "です。");
+
+
+for (let i = 0; i <= holidays.length; i++) {
+    console.log("本日は" + holidays[i] + "です。");
+}
