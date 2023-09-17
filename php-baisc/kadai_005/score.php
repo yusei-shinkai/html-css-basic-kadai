6cbf2d72f1b230ebb51a0f5839ac91c5b9d16064
yusei-shinkai <140622972+yusei-shinkai@users.noreply.github.com>
2023-09-17 19:39:04 +0900

    php-baisc.kadai_005をアップロード

--- php-baisc/kadai_005/score.php
+++ php-baisc/kadai_005/score.php
@@ -1,28 +1,16 @@
  <?php 
     $score1 = 80 ;
-    //echo $score1;
     $score2 = 60 ;
-    //echo $score2;
     $score3 = 55 ;
-    //echo $score3;
     $score4 = 40 ;
-    //echo $score4;
     $score5 = 100 ;
-    //echo $score5;
     $score6 = 25 ;
-    //echo $score6;
     $score7 = 80 ;
-    //echo $score7;
     $score8 = 95 ;
-    //echo $score8;
     $score9 = 30 ;
-    //echo $score9;
     $score10 = 60 ;
-    //echo $score10;
     $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
     $ave = $total /10;
     echo $ave
-    //echo $average;
-    //echo $average /10;
 ?>
   
